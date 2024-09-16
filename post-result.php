4408f186eb5bf3df78b467d619c8928516a55242
ClaireYarber <clyarber@ou.edu>
2024-09-15 20:20:50 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,15 +4,14 @@ Include "view-header.php";
 ?>
 <h1>Post result</h1>
 <?PHP
-if (isset($_POST['my-name'])) {
-?>
-  <p>The value sent is:</p>
-<?php
-  echo $_POST['my-name'];
+echo getDisplay();
+include "view-footer.php";
+
+function getDisplay() {
+  if (isset($_POST['my-name'])) {
+    return "<p>The value sent is:</p>" . $_POST['my-name'];
 } else { 
-?>
-  <p>Nothing posted to the page.</p>
-<?php
+    return "<p>The value sent is:</p>";
 }
-Include "view-footer.php";
+}
 ?>
